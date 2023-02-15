247a094e4dfa736bb470a80d839cfbb710591934
Shashank <97978650+shash0708@users.noreply.github.com>
2023-02-15 21:22:27 +0530

    Add files via upload
    this is an e-commerce website on bikes (front end),this is my first commit.

---
+++ data.js
@@ -0,0 +1,70 @@
+var product=[
+    {
+        "id":0,
+        "title":"",
+        "brand":"",
+        "price":"",
+        "Mileage":"",
+        "Engine":"",
+        "weight":"",
+        "Body Frame Material":"",
+        "image":"https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930"
+        "Ratings":"https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930"},
+        {
+            "id":1,
+            "title":" jawa peralc",
+            "brand":"jawa",
+            "price":"2.75 lakhs",
+            "Mileage":"30km/L",
+            "Engine":"334cc",
+            "Weight":"175kg",
+            "Body Frame Material":"Steel",
+            "image":"url(i/a.png)"
+            "Ratings":"url(i/w1.jpeg)"},
+            {
+            "id":2,
+            "title":"BMW G 310 R",
+            "brand":"BMW",
+            "price":"3.10 lakhs",
+            "Mileage":"32km/L",
+            "Engine":"313cc",
+            "Weight":"164kg",
+            "Body Frame Material":"Steel",
+            "image":"url(i/b.png)"
+            "Ratings":"url(i/w2.jpeg)"},
+            {
+            "id":3,
+            "title":"Kawasaki Ninja ZX-10R",
+            "brand":"Kawasaki",
+            "price":"17.85 lakhs",
+            "Mileage":"15km/L",
+            "Engine":"998cc",
+            "Weight":"207kg",
+            "Body Frame Material":"Aluminium",
+            "image":"url(i/d.png)"
+            "Ratings":"url(i/w3.jpeg)"},
+
+            {
+            "id":4,
+            "title":"Harley Davidson Iron 883",
+            "brand":"Harley Davidson",
+            "price":"11.9 lakhs",
+            "Mileage":"12 to 17km/L",
+            "Engine":"883cc",
+            "Weight":"228kg",
+            "Body Frame Material":"Iron",
+            "image":"url(i/c.png)"
+            "Ratings":"url(i/w4.jpeg)"},
+            {
+            "id":5,
+            "title":"Triumph Bonneville T100",
+            "brand":"Triumph",
+            "price":"10.68 lakhs",
+            "Mileage":"24km/L",
+            "Engine":"900cc",
+            "Weight":"228kg",
+            "Body Frame Material":"Steel",
+            "image":"url(i/e.png)"
+            "Ratings":"url(i/w5.jpeg)"},
+            ]    
+
